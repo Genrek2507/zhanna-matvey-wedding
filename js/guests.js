ad15c67a809e565f1d8170b6c232e99bd37ab39e
Genrek2507 <zhanekrapivina@yandex.ru>
2026-08-08 12:04:55 +0300

    Add files via upload

---
+++ js/guests.js
@@ -0,0 +1,63 @@
+const guests = {
+
+    "marina-andrey-sasha": {
+        name: "Марина, Андрей и Сашенька",
+        text: "Будем очень рады разделить этот особенный день вместе с вами!",
+        qr: "qr/marina-andrey-sasha.png"
+    },
+
+    "sofia": {
+        name: "Софья",
+        text: "Будем очень рады видеть тебя на нашей свадьбе!",
+        qr: "qr/sofia.png"
+    },
+
+    "danila": {
+        name: "Данила",
+        text: "Будем очень рады видеть тебя на нашей свадьбе!",
+        qr: "qr/danila.png"
+    },
+
+    "evgenia": {
+        name: "Евгения",
+        text: "Будем очень рады видеть тебя на нашей свадьбе!",
+        qr: "qr/evgenia.png"
+    },
+
+    "angelina": {
+        name: "Ангелина",
+        text: "Будем очень рады видеть тебя на нашей свадьбе!",
+        qr: "qr/angelina.png"
+    },
+
+    "lubov": {
+        name: "Любовь",
+        text: "Будем очень рады видеть тебя на нашей свадьбе!",
+        qr: "qr/lubov.png"
+    },
+
+    "daria-pavel": {
+        name: "Дарья и Павел",
+        text: "Будем очень рады видеть вас на нашей свадьбе!",
+        qr: "qr/daria-pavel.png"
+    },
+
+    "luka": {
+        name: "Лука",
+        text: "Будем очень рады видеть тебя на нашей свадьбе!",
+        qr: "qr/luka.png"
+    },
+
+    "maksim-veronika": {
+        name: "Максим и Вероника",
+        text: "Будем очень рады видеть вас на нашей свадьбе!",
+        qr: "qr/maksim-veronika.png"
+    },
+
+    "volodya-natasha": {
+        name: "Володя и Наташа",
+        text: "Будем очень рады видеть вас на нашей свадьбе!",
+        qr: "qr/volodya-natasha.png"
+    }
+
+};
